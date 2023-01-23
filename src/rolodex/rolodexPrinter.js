8794a3b72830b60437192e28a42c16a759f6ee09
Nandini Rao [nandinirao] <nandinirao@costco.com>
2023-01-23 11:11:10 -0800

    Removed .js and assert to get lint to pass.

--- src/rolodex/rolodexPrinter.js
+++ src/rolodex/rolodexPrinter.js
@@ -1,20 +1,10 @@
-import people from './people.json' assert { type: "json" };
-
-// people.forEach((person) => {
-//   const names = person.name.split(' ');
-//   const firstName = names[0];
-//   const lastName = names[1];
-//   const { email } = person;
-//   const { phone } = person;
-
-//   console.log(`First name: ${firstName}\nLast name: ${lastName}\nEmail: ${email}\nPhone number: ${phone}\n`);
-// });
+import people from './people.json';
 
 // Refactored code in ES6 syntax
 people.forEach((person) => {
   // Check for title like Mrs.
-  const [namePart1, namePart2] = person.name?.split('. ');
-  const [firstName, lastName] = namePart2 ? namePart2?.split(' ') : namePart1?.split(' ');
+  const [namePart1, namePart2] = person.name.split('. ');
+  const [firstName, lastName] = namePart2 ? namePart2.split(' ') : namePart1.split(' ');
   const { email, phone } = person;
 
   console.debug(`****${person.name}****`);
